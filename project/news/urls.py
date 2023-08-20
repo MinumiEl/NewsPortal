e54aa130e92f31b1f625b43db87d23dad5cbeb07
MinumiEl <eeydlin@bk.ru>
2023-08-20 17:07:07 +0300

    Add files via upload

--- project/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostList, PostsDetail
-
-
-urlpatterns = [
-   path('news/', PostList.as_view()),
-   path('post/<int:pk>/', PostsDetail.as_view()),
-]
